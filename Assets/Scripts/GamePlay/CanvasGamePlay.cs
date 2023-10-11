8c78351613cb9a090d99bf7abdd40a18b2409d30
Dai_Di_Bar <Dai_Di_Bar@DESKTOP-UK0KLJD>
2023-10-11 16:06:15 +0700

    dai

--- Assets/Scripts/GamePlay/CanvasGamePlay.cs
+++ Assets/Scripts/GamePlay/CanvasGamePlay.cs
@@ -8,18 +8,32 @@ using UnityEngine.UI;
 public class CanvasGamePlay : MonoBehaviour
 {
     [SerializeField] private Sprite[] ob_layer;
-    [SerializeField] private GameObject panel_gameMode,loadTime;
+    [SerializeField] private GameObject panel_gameMode,loadTime, panel_maintenance;
     private int index_Scene = -1,time=3;
     public void playNow()
     {
         panel_gameMode.SetActive(true);
     }
+   IEnumerator maintenance()
+    {
+        panel_maintenance.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        panel_maintenance.SetActive(false);
+    }
     public void setSelectGame(int index)
     {
-        index_Scene = index;
-        panel_gameMode.SetActive(false);
-        loadTime.SetActive(true);
-        InvokeRepeating(nameof(loadTiming),0,1);
+        if (index > 2)
+        {
+            StartCoroutine(maintenance());
+        }
+        else
+        {
+            index_Scene = index;
+            panel_gameMode.SetActive(false);
+            loadTime.SetActive(true);
+            InvokeRepeating(nameof(loadTiming), 0, 1);
+        }
+       
       
     }
     public void loadTiming()
